b632c651d289107e897b944974aa4229ff1209c7
Emel <emel.garouachi@esprit.tn>
2018-10-25 11:13:28 +0100

    a

---
+++ WEBB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEBB.Startup))]
+namespace WEBB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
