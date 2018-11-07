58d3f0969127405b1789e6f0493634377a40c4ff
Emel <emel.garouachi@esprit.tn>
2018-11-07 18:51:55 +0100

    login

--- Data/PiContext.cs
+++ Data/PiContext.cs
@@ -31,7 +31,7 @@ namespace Data
         public DbSet<Debreif> Debreifs { get; set; }
         public DbSet<Repport> Repports { get; set; }
         public DbSet<Rate> Rates { get; set; }
-
+       // public DbSet<Step> Steps { get; set; }
 
 
 
