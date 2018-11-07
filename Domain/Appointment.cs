68ef3004108e8657bfc6c29f13ce85b1862de1f9
Emel <emel.garouachi@esprit.tn>
2018-11-07 11:26:36 +0100

    identiti

--- Domain/Appointment.cs
+++ Domain/Appointment.cs
@@ -15,7 +15,8 @@ namespace Domain
         public State state { get; set; }
         public virtual Patient patient { get; set; }
         public virtual Doctor doctor { get; set; }
-        public virtual Course course { get; set; }
+      //  public virtual Course course { get; set; }
+        public ICollection<Course> Courses { get; set; }
 
     }
 }
